9e08fdd64821974a73dcd088a5aa7d35a08cd438
bryanpiguave <bpiguave@espol.edu.ec>
2023-12-20 11:37:35 -0500

    chore: flake8 server.py

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
 from typing import Union
 from fastapi import FastAPI
-import torch 
+import torch
 import argparse
 import uvicorn
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", type=str, default="export_model", help="Model file name")
+parser.add_argument("--model", type=str,
+                    default="export_model", help="Model file name")
 parser.add_argument("--port", type=int, default=8000, help="Port number")
 args = parser.parse_args()
 
@@ -16,6 +17,7 @@ app = FastAPI()
 This server receives an image and gives a prediction.
 """
 
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
@@ -25,7 +27,6 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=args.port)
-
-
