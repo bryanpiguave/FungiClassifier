3823a1b61193c1af41c8f7a5255c26d18fc5431c
bryanpiguave <bpiguave@espol.edu.ec>
2023-12-20 10:54:58 -0500

    feat: adding server structure

---
+++ server.py
@@ -0,0 +1,31 @@
+from typing import Union
+from fastapi import FastAPI
+import torch 
+import argparse
+import uvicorn
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str, default="export_model", help="Model file name")
+parser.add_argument("--port", type=int, default=8000, help="Port number")
+args = parser.parse_args()
+
+app = FastAPI()
+
+
+"""
+This server receives an image and gives a prediction.
+"""
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=args.port)
+
+
